cd854312d11429225c76ab785ba163fecb7716ea
takish <chipa34@gmail.com>
2017-03-01 14:11:06 +0900

    Initial Commit

---
+++ camera_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  camera_test
+//
+//  Created by 岸田　崇志 on 2017/03/01.
+//  Copyright © 2017年 takish.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
